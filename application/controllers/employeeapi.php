36e88161436b3a752e10fc9b31f3b12472ffb735
jewekyut <jewemalubay@gmail.com>
2026-04-24 09:58:54 +0800

    Employee API complete - all CRUD endpoints working

--- application/controllers/employeeapi.php
+++ application/controllers/employeeapi.php
@@ -12,7 +12,6 @@ class Employeeapi extends CI_Controller {
     public function addEmployee() {
         $this->load->database();
         $data = json_decode(file_get_contents("php://input"), true);
-
         $employee = array(
             'emp_firstname'  => $data['emp_firstname'],
             'emp_lastname'   => $data['emp_lastname'],
@@ -25,9 +24,7 @@ class Employeeapi extends CI_Controller {
             'gross_salary'   => $data['gross_salary'],
             'emp_sys_status' => 1
         );
-
         $this->db->insert('employees', $employee);
-
         if ($this->db->affected_rows() > 0) {
             echo json_encode(["status" => "success", "id" => $this->db->insert_id()]);
         } else {
@@ -38,7 +35,6 @@ class Employeeapi extends CI_Controller {
     public function updateEmployee() {
         $this->load->database();
         $data = json_decode(file_get_contents("php://input"), true);
-
         $employee = array(
             'emp_firstname'  => $data['emp_firstname'],
             'emp_lastname'   => $data['emp_lastname'],
@@ -50,10 +46,8 @@ class Employeeapi extends CI_Controller {
             'department'     => $data['department'],
             'gross_salary'   => $data['gross_salary']
         );
-
         $this->db->where('emp_id', $data['emp_id']);
         $this->db->update('employees', $employee);
-
         if ($this->db->affected_rows() > 0) {
             echo json_encode(["status" => "success"]);
         } else {
@@ -64,10 +58,8 @@ class Employeeapi extends CI_Controller {
     public function deleteEmployee() {
         $this->load->database();
         $data = json_decode(file_get_contents("php://input"), true);
-
         $this->db->where('emp_id', $data['emp_id']);
         $this->db->delete('employees');
-
         if ($this->db->affected_rows() > 0) {
             echo json_encode(["status" => "success"]);
         } else {
